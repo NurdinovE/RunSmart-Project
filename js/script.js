bf9010771568aac684a867b227c423e985637aa1
NurdinovE <nurdinovvv06@gmail.com>
2022-10-27 11:14:38 +0600

    adding some changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-    $(document).ready(function(){
+$(document).ready(function(){
     $('.carousel__inner').slick({
         autoplay: true,
         speed: 1200,
         prevArrow: "<button type=\"button\" class=\"slick-prev\"><</button>",
         nextArrow: "<button type=\"button\" class=\"slick-next\">></button>"
-    })
+    });
         $('ul.catalog__tabs').on('click', 'li:not(catalog__tabs_active)', function() {
             $(this)
                 .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
@@ -12,30 +12,31 @@
         });
     $('.catalog-item__link').each(function (i) {
             $(this).on('click', function (e) {
-                e.preventDefault()
-                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active')
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
             })
-        })
+        });
         $('.catalog-item__back').each(function (i) {
             $(this).on('click', function (e) {
-                e.preventDefault()
-                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active')
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
             })
-        })
+        });
         $('[data-modal=consultation]').on('click', function () {
             $('.overlay, #consultation').fadeIn()
-        })
+        });
         $('.modal__close').on('click', function () {
             $('.overlay, #consultation, #order, #thanks').fadeOut()
-        })
+        });
         $('.btn_mini').on('click', function () {
             $('.overlay, #order').fadeIn()
-        })
+        });
         $('.button_submit').on('click', function () {
+            $('.overlay, #order').fadeOut();
             $('.overlay, #thanks').fadeIn()
-        })
+        });
         $(function(){
             $("#phone").mask("+999(999) 99-99-99");
         });
@@ -46,5 +47,5 @@
             $('.pageup').fadeOut()
 
     })
-    });
+});
 
